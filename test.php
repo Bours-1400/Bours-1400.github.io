01e6d67f0f55db382510c34589f624fab15057f6
Bours-1400 <83705088+Bours-1400@users.noreply.github.com>
2021-06-05 17:34:50 +0430

    Update and rename test.html to test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$name=$_POST['name'];
+$lastname=$_POST['lastname'];
+$number=$_POST['number'];
+
+$file=fopen("main.txt","a+");
+fwrite($file,"hello")
+
+// $conn=new mysqli("localhost","root","","form1");
+// if (mysqli_connect_error()){
+//     echo "Faild:". mysqli_connect_error();
+
+// }
+// mysqli_query($conn,"INSERT INTO `form1`(`Name`, `Last Name`, `Mobile Number`) VALUES ('$name','$lastname','$number')");
+
+// $conn->close()
+
+?>
